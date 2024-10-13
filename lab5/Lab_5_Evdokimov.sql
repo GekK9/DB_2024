89deaf3a355a15d8675b25e642e275b5d6d2fcc7
GekK9 <Nikita.evdokimov3234@gmail.com>
2024-10-14 02:44:17 +0300

    for 24/25 semester

--- lab5/Lab_5_Evdokimov.sql
+++ lab5/Lab_5_Evdokimov.sql
@@ -1,24 +1,37 @@
-USE LAB5;
+USE master;
 GO
 
-IF OBJECT_ID(N'Client') IS NOT NULL
-	DROP TABLE Client;
+IF DB_ID (N'lab5') IS NOT NULL
+	DROP DATABASE lab5;
 GO
 
-CREATE TABLE Client
+CREATE DATABASE lab5
+	ON (NAME = lab5_dat, FILENAME = "C:\BD\lab5\lab5dat.mdf",
+		SIZE = 10, MAXSIZE = UNLIMITED, FILEGROWTH = 5%)
+	LOG ON (NAME = lab5_log, FILENAME = "C:\BD\lab5\lab5log.ldf",
+		SIZE = 5MB, MAXSIZE = 25MB, FILEGROWTH = 5MB); 
+GO
+
+USE lab5;
+GO
+
+IF OBJECT_ID(N'Players') IS NOT NULL
+	DROP TABLE Players;
+GO
+
+CREATE TABLE Players 
 	(
-	Client_code INT PRIMARY KEY NOT NULL ,
-	Name NVARCHAR(80) NOT NULL,
-	ADDRESS NCHAR(80) UNIQUE NOT NULL ,
-	Phone_number INT UNIQUE NOT NULL
+	User_login varchar(20) PRIMARY KEY NOT NULL,
+	Email varchar(254) UNIQUE NOT NULL,
+	Donate_points int NOT NULL,
+	Registration_Date datetime NOT NULL
 	)
 GO
 
-ALTER DATABASE Lab5
-	ADD FILEGROUP Lab5_filegroup;
+ALTER DATABASE lab5 
+	ADD FILEGROUP lab5_filegroup;
 GO
-
-ALTER DATABASE Lab5
+ALTER DATABASE lab5
 	ADD FILE 
 	(
 	NAME = 'alter_lab5_dat',
@@ -26,113 +39,70 @@ ALTER DATABASE Lab5
 	SIZE = 5MB,
 	MAXSIZE = UNLIMITED,
 	FILEGROWTH = 5%)
-TO FILEGROUP Lab5_filegroup;
+TO FILEGROUP lab5_filegroup;
 GO
 
-ALTER DATABASE Lab5
-MODIFY FILEGROUP Lab5_filegroup DEFAULT;
+ALTER DATABASE lab5
+MODIFY FILEGROUP lab5_filegroup DEFAULT;
 GO
 
-
-IF OBJECT_ID(N'Product') is NOT NULL
-	DROP TABLE Product	
+IF OBJECT_ID(N'Maps') IS NOT NULL
+	DROP TABLE Maps;
 GO
 
-CREATE TABLE Product 
+CREATE TABLE Maps
 	(
-	Item_number int PRIMARY KEY NOT NULL,
-	Name_of_product NCHAR(50) UNIQUE NOT NULL,
-	Price INT,
-	);
+	map_code int PRIMARY KEY NOT NULL IDENTITY(1,1),
+	map_name varchar(45) NOT NULL UNIQUE
+	)
 GO
 
+select * from sys.filegroups
 
-
-INSERT INTO Product
-VALUES 
-(1, 'Milk', 10),
-(2, 'bread', 24)
-GO
-
-
-ALTER DATABASE Lab5
-	ADD FILEGROUP Lab5_1_filegroup;
-GO
-
-ALTER DATABASE Lab5
-	ADD FILE 
-	(
-	NAME = 'alter_lab5_1_dat',
-	FILENAME = 'C:\BD\lab5\alter_lab5_1_dat.ndf',
-	SIZE = 5MB,
-	MAXSIZE = UNLIMITED,
-	FILEGROWTH = 5%)
-TO FILEGROUP Lab5_1_filegroup;
-GO
-
-
-ALTER DATABASE Lab5
-MODIFY FILEGROUP Lab5_1_filegroup DEFAULT;
-GO
-
-IF OBJECT_ID(N'Product_copy') is NOT NULL
-	DROP TABLE product_copy;
-GO
-
-SELECT *
-INTO dbo.product_copy 
-FROM Product  
-GO  
-
-SELECT * FROM dbo.product_copy;
-GO
-
-sp_help product
-
-DROP TABLE product;
-GO
-
-
-select * from sys.filegroups;
+select * from Maps;
+select * from Players;
 GO
 
 ALTER DATABASE Lab5
 	MODIFY	FILEGROUP [primary] DEFAULT;
 GO
 
+IF OBJECT_ID(N'maps_copy') is NOT NULL
+	DROP TABLE maps_copy;
+GO
+
+SELECT *
+INTO dbo.Maps_copy 
+FROM Maps  
+GO  
+
 alter database lab5
-    remove file alter_lab5_dat;
+    remove file alter_lab5_dat
+	
 go
+
+drop table maps;
+go
+
 alter database lab5
     remove filegroup Lab5_filegroup;
 go
 
-sp_help product_copy
-
-Select * from product_copy;
+select * from sys.filegroups
 GO
+--sp_help Maps;
+--GO
+--sp_help Players;
 
-
-DROP TABLE product_copy;
-GO
-
-alter database lab5
-    remove file alter_lab5_1_dat;
-go
-alter database lab5
-    remove filegroup Lab5_1_filegroup;
-go
 
 CREATE SCHEMA Lab5_schema;
 GO
 
-ALTER SCHEMA Lab5_schema TRANSFER Client;
+ALTER SCHEMA Lab5_schema TRANSFER maps_copy;
 GO
 
-DROP TABLE lab5_schema.Client;
+DROP TABLE lab5_schema.maps_copy;
 GO
 
 DROP SCHEMA lab5_schema;
 GO
-
-select * from sys.filegroups;
